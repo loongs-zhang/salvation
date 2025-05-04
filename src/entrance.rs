3418c053877ee43ab73cc65f63518c10ca130576
dragon-zhang <zhangzicheng@apache.org>
2025-05-04 20:29:06 +0800

    add scene transition

--- src/entrance.rs
+++ src/entrance.rs
@@ -1,6 +1,9 @@
 use crate::world::RustWorld;
-use godot::classes::{Button, Control, IButton, IControl, Object, PackedScene, VBoxContainer};
+use godot::classes::{
+    Button, ColorRect, Control, IButton, IControl, Object, PackedScene, VBoxContainer,
+};
 use godot::obj::{Base, Gd, OnReady, WithBaseField};
+use godot::prelude::ToGodot;
 use godot::register::{GodotClass, godot_api};
 
 #[derive(GodotClass)]
@@ -54,6 +57,28 @@ impl EndlessMode {
 
     #[func]
     pub fn on_endless_mode_pressed(&mut self) {
+        self.base_mut().set_visible(false);
+        let container = self.base().get_parent().expect("VBoxContainer not found");
+        container
+            .get_node_as::<ExitGame>("ExitGame")
+            .set_visible(false);
+        let color_rect = container
+            .get_parent()
+            .expect("RustEntrance not found")
+            .get_node_as::<ColorRect>("ColorRect");
+        let mut tween = self
+            .base_mut()
+            .create_tween()
+            .expect("Failed to create tween");
+        tween
+            .tween_property(&color_rect, "modulate:a", &1_f32.to_variant(), 2.0)
+            .unwrap()
+            .from(&0_f32.to_variant());
+        tween.tween_callback(&self.base_mut().callable("change_scene"));
+    }
+
+    #[func]
+    pub fn change_scene(&mut self) {
         if let Some(world) = self.world_scene.try_instantiate_as::<RustWorld>() {
             if let Some(tree) = self.base().get_tree() {
                 if let Some(mut root) = tree.get_root() {
